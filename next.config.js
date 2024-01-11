e44086c6c31bd282176975034a944febbcb5fc84
0nizzr <nizzrdev@gmail.com>
2024-01-11 18:01:27 +0100

    disable caching for /blog/[slug]

--- next.config.js
+++ next.config.js
@@ -3,4 +3,17 @@ module.exports = {
   typescript: {
     ignoreBuildErrors: true,
   },
+  async headers() {
+    return [
+      {
+        source: "/blog/[slug]",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0",
+          },
+        ],
+      },
+    ];
+  },
 };
